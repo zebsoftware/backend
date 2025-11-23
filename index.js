de6eec1222c0d26b9466e1fa1734f53ef172242e
Anwar zeb <anwarzeb2006@gmail.com>
2025-11-23 23:42:08 +0500

    update

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get("/", (req, res) => {
 });
 
 // Server Start
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
