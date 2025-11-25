b7a7b42c444c919141d940a8d07c4dee54291d78
Anwar zeb <anwarzeb2006@gmail.com>
2025-11-25 22:37:31 +0500

    added

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { sendContact } from "../controllers/contactController.js";
+import { getAllContacts, sendContact } from "../controllers/contactController.js";
 
 const router = express.Router();
 
 // POST /api/contact
 router.post("/", sendContact);
+router.get("/", getAllContacts);
 
 export default router;
