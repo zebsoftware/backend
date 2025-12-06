615038a6d8bce99e9b5042511e530fadb4d5b874
Anwar zeb <anwarzeb2006@gmail.com>
2025-12-06 19:18:59 +0500

    aded

--- routes/product.js
+++ routes/product.js
@@ -4,10 +4,10 @@ import { upload } from "../middleware/upload.js";
 
 const router = express.Router();
 
-// All routes
-router.get("/products", getAllProducts);
-router.post("/products", upload.single("image"), addProduct);
-router.put("/products/:id", upload.single("image"), updateProduct);
-router.delete("/products/:id", deleteProduct);
+// All routes relative to /api/products
+router.get("/", getAllProducts);
+router.post("/", upload.single("image"), addProduct);
+router.put("/:id", upload.single("image"), updateProduct);
+router.delete("/:id", deleteProduct);
 
 export default router;
